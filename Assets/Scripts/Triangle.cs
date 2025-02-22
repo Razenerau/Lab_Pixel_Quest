1f310b4e3d86afd9e9ec196096dea97caa5351ec
Razenerau <molokov.phil@gmail.com>
2025-02-22 12:07:01 -0500

    can't fix jumping problem
    can't fix jumping problem

--- Assets/Scripts/Triangle.cs
+++ Assets/Scripts/Triangle.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Triangle : MonoBehaviour
 {
     [SerializeField] private const int _speed = 7;
-    [SerializeField] private const float _JUMPSTRENGTH = 0.05f;
+    [SerializeField] private const float _JUMPSTRENGTH = 0.04f;
     public Rigidbody2D rb;
 
     private float xInput;
@@ -16,6 +17,15 @@ public class Triangle : MonoBehaviour
     private float _jumpCooldown = 1f;
     private float _currentJumpCooldown = 1f;
     private bool _canJump = true;
+    private bool _isJumping = false;
+
+    // Tags
+    private const string _GROUNDTAG = "Ground";
+    private const string _ENEMYTAG = "Enemy";
+    private const string _FINISHTAG = "Finish";
+
+    //Levels
+    private const string _NEXT_LEVEL = "Level 2";
 
     // Start is called before the first frame update
     void Start()
@@ -50,6 +60,7 @@ public class Triangle : MonoBehaviour
         switch (key)
         {
             case 'W':
+                
                 break;
             case 'S':
                 break;
@@ -69,7 +80,61 @@ public class Triangle : MonoBehaviour
     {
         xInput = Input.GetAxis("Horizontal");
         yInput = Input.GetAxis("Vertical");
-        Debug.Log(xInput);
-        rb.velocity = new Vector2(xInput * _speed, rb.velocity.y + (yInput) * _JUMPSTRENGTH);
+        //Debug.Log(yInput);
+        rb.velocity = new Vector2(xInput * _speed, rb.velocity.y);
+        jump();
+        if(_isJumping) handleJumpCooldown();
+        if(Input.GetKeyDown(KeyCode.W) && _canJump)
+        {
+            _currentJumpCooldown = _jumpCooldown;
+        }
+        if(!Input.GetKey(KeyCode.W))
+        {
+            _isJumping = false;
+        }
+
+    }
+
+    private void handleJumpCooldown()
+    {
+        Debug.Log("Countdown Jump: " + _currentJumpCooldown);
+        if (_currentJumpCooldown <= 0)
+        {
+            Debug.Log("Disable Jump");
+            _canJump = false;
+        }
+        else
+        {
+            //Debug.Log("Countdown Jump: " + _currentJumpCooldown);
+            _currentJumpCooldown -= Time.deltaTime;
+        }
+    }
+
+    private void jump()
+    {
+        if(_canJump && yInput > 0)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y + (yInput) * _JUMPSTRENGTH);
+            _isJumping = true;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case _GROUNDTAG:
+                Debug.Log("Player collides with " + collision.gameObject.tag);
+                _isJumping = false;
+                _canJump = true;
+                break;
+            case _ENEMYTAG:
+                string thisLevel = SceneManager.GetActiveScene().name;
+                SceneManager.LoadScene(thisLevel);
+                break;
+            case _FINISHTAG:
+                SceneManager.LoadScene(_NEXT_LEVEL);
+                break;
+        }
     }
 }
